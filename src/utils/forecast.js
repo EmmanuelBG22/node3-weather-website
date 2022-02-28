5486fbf8681b60cc608994f60ebc2062199ed23b
Olajumoke Emmanuel <emmanuel.olajumoke@babbangona.com>
2022-02-28 15:27:22 +0100

    forecast edited

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=5ca91cfdd85542f063a70abb42a9e5bd&query=37.8267,-122.4233&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=5ca91cfdd85542f063a70abb42a9e5bd&query=' + latitude + ',' + longitude + '&units=f'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
